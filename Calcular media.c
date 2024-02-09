214a82f7aa8a3a8afd1331337635a3df545fd553
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-02-09 10:08:41 -0300

    Create Calcular media.c

---
+++ Calcular media.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+    float nota1, nota2, nota3, nota4, media;
+
+    // Solicita as quatro notas do usuário
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+    printf("Digite a terceira nota: ");
+    scanf("%f", &nota3);
+    printf("Digite a quarta nota: ");
+    scanf("%f", &nota4);
+
+    // Calcula a média das notas
+    media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    // Exibe a média
+    printf("A média do aluno(a) é: %.2f\n", media);
+
+    return 0;
+}
