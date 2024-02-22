568ebb1d748bf82a93b4197fce1ee5b5968f1901
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-02-22 14:11:47 -0300

    Update 4operacoesdoisnumeros.c

--- 4operacoesdoisnumeros.c
+++ 4operacoesdoisnumeros.c
@@ -1,1 +1,45 @@
+include<stdio.h>
+#include<locale.h>
+int main(){
+setlocale(LC_ALL,"portuguese");
+//Declaração das variáveis.
+char primeiroNumero,segundoNumero;
+int soma,subtracao,multiplicacao,divisao;
+//Peça os valores ao usuário.
+printf("\nDigite o primeiro numero:");
+scanf("%c",&primeiroNumero);
+
+printf("\nDigite o segundo numero:");
+scanf("%c",&segundoNumero);
+
+//Calculando as 4 operações  com o primeiro e segundo números.
+soma=primeiroNumero+segundoNumero;
+subtracao=primeiroNumero-segundoNumero;
+multiplicacao=primeiroNumero*segundoNumero;
+divisao=primeiroNumero/segundoNumero;
+//Exibindo Resultado.
+printf("\n==Exibindo Resultados:");
+printf("\nSoma:",soma);
+printf("\nSubtração":subtracao);
+printf("\nMultiplicação:",multiplicacao);
+printf("\nDivisão:",divisao);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
