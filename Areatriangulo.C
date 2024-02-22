aa1d1d7b767c1744174533462d7690319fe73ec6
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-02-22 14:36:55 -0300

    Create Areatriangulo.C

---
+++ Areatriangulo.C
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+int main(){
+int base,area,altura;
+printf("\nDigite a base do triângulo em cm:");
+scanf("%i",&base);
+
+printf("\nDigite a altura  do triângulo em cm:");
+scanf("%i",&altura);
+
+area=base*altura;
+
+printf("\nA área do triângulo é:%i",area);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
