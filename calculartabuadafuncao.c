4ade3c3407aaf3f7dc72a7efcc74dfede7e9772d
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-03-05 17:20:12 -0300

    Create calculartabuadafuncao.c

---
+++ calculartabuadafuncao.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+// Função para exibir a tabuada de um número
+void exibirTabuada(int numero) {
+int i;
+
+  // Laço para calcular e exibir a tabuada de 1 a 10
+  for (i = 1; i <= 10; i++) {
+    printf("%d x %d = %d\n", numero, i, numero * i);
+  }
+}
+
+int main() {
+  int numero;
+  int i;
+  // Leitura do número do usuário
+  printf("Digite um número: ");
+  scanf("%d", &numero);
+
+  // Chamada da função para exibir a tabuada
+  exibirTabuada(numero);
+
+  return 0;
+}
